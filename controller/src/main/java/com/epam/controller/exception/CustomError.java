cbe9647d7394927df1c3c020a398483bb4ee7a57
IseTheSal <illya.ageevich@gmail.com>
2021-06-07 22:52:56 +0300

    first commit

---
+++ controller/src/main/java/com/epam/controller/exception/CustomError.java
@@ -0,0 +1,28 @@
+package com.epam.controller.exception;
+
+public class CustomError {
+
+    private String errorMessage;
+    private int errorCode;
+
+    public CustomError(String errorMessage, int errorCode) {
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+}
