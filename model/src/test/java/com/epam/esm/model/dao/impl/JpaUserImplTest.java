2d24f513f376698580c2c118a9c8a8683c6b1953
IseTheSal <illya.ageevich@gmail.com>
2021-07-04 00:21:22 +0300

    Module 3. Part 3. FIX

---
+++ model/src/test/java/com/epam/esm/model/dao/impl/JpaUserImplTest.java
@@ -0,0 +1,38 @@
+package com.epam.esm.model.dao.impl;
+
+import com.epam.esm.model.dao.UserDao;
+import com.epam.esm.model.dao.config.SpringBootTestConfiguration;
+import com.epam.esm.model.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.List;
+
+@ContextConfiguration(classes = {JpaUserImpl.class, SpringBootTestConfiguration.class},
+        loader = AnnotationConfigContextLoader.class)
+@DirtiesContext
+@SpringBootTest
+public class JpaUserImplTest {
+
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    void findById() {
+        boolean condition = userDao.findById(1).isPresent();
+        Assertions.assertTrue(condition);
+    }
+
+    @Test
+    void findAll() {
+        List<User> all = userDao.findAll(100, 0);
+        boolean condition = (all.size() >= 2);
+        Assertions.assertTrue(condition);
+    }
+
+}
