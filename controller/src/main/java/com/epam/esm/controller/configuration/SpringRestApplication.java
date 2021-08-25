cca1bbe4cb90d69db5ecc98c71706ae4af1e87af
IseTheSal <illya.ageevich@gmail.com>
2021-08-25 13:12:49 +0300

    Module 6. The latest version

--- controller/src/main/java/com/epam/esm/controller/configuration/SpringRestApplication.java
+++ controller/src/main/java/com/epam/esm/controller/configuration/SpringRestApplication.java
@@ -3,6 +3,8 @@ package com.epam.esm.controller.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,9 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.epam.esm")
 @EntityScan(basePackages = "com.epam.esm")
 @EnableJpaRepositories(basePackages = "com.epam.esm")
-public class SpringRestApplication {
+public class SpringRestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringRestApplication.class);
     }
 }
